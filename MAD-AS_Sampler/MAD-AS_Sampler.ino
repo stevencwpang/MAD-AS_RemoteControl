64788db0983c82ccdf979688f6adf39f373e7ac3
stevencwpang <76076724+stevencwpang@users.noreply.github.com>
2022-03-29 13:57:48 +1100

    V0.0.2
    Added HC-12 sleep mode

--- MAD-AS_Sampler/MAD-AS_Sampler.ino
+++ MAD-AS_Sampler/MAD-AS_Sampler.ino
@@ -1,12 +1,9 @@
-//This program is used to check the command on the cloud website and communicate the command to the nearby device through radio modules.
+//This program is used to check the command over the radio module and respond with a message.
 
 //Include all necessary libraries:
 #include <SoftwareSerial.h>
 #include <avr/power.h>
 #include <LowPower.h>
-
-//Device specific config
-#define DEVICEID "MAD-AS Testing"
 
 //For BoSL/Arduino Board
 #define BAUDRATE 9600
@@ -19,7 +16,6 @@ SoftwareSerial HC12(HC_TX, HC_RX); //Radio Module HC-12
 
 //Variables Declaration
 volatile int WebCmd = 0; //Command from the cloud website: 0 means no sampling, 1 means start sampling.
-volatile int DeviceWakeUp = 0; //If the nearby device receives wake-up command (start sampling): 0 means no response from nearby device, 1 means response given by nearby device.
 extern volatile unsigned long timer0_millis; //millis timer variable 
 
 void setup() {
@@ -34,61 +30,54 @@ void setup() {
 }
 
 void loop() {
-  //Repeatedly check command from the cloud every ten seconds, until the command is YES.
+  //Repeatedly check command from the radio every 10 seconds, until the command is YES.
   while (WebCmd == 0){
-    Serial.println("Start checking command from the cloud...");
-    CheckWebCmd();
+    Serial.println("Start checking command from the radio...");
+    CheckRadioCmd();
     if (WebCmd == 0){
-      Serial.println("No start command is given from the cloud. Next check starts in 10 seconds.");
-      Sleepy(10);
+      Serial.println("No start command is given from the radio. Next check starts in 10 seconds.");
     }else{
-      Serial.println("Command to start sampling is received from the cloud.");
-      WakeUp();
-      if (DeviceWakeUp != 0){
-        Serial.println("The nearby device has received the command and its response is now capture.");
-        Serial.println("This program ends.");
-        Sleepy(0);
-      }else{
-        Serial.println("Something went wrong if you see this message!!!!!");
-        Serial.println("This program ends.");
-        Sleepy(0);
-      }
+      Serial.println("Command to start sampling is received from the radio.");
+      Respond();
+      break;
     }
+    Sleepy(10);
   }
+  Serial.println("can start the rest of program now...");
+  Sleepy(0);
 }
 
-
-
-//Check command from the cloud
-void CheckWebCmd(){
-  ////////////////////////////////////////////
-  WebCmd = 1;
-}
-
-//Wake up the nearby device to start sampling
-void WakeUp(){
-  int i,j;
+//Check command from the radio
+void CheckRadioCmd(){
+  int j;
   byte x;
   HC12.print("AT+DEFAULT");    // Send AT Command to HC-12
   digitalWrite(HC_SET,HIGH); //Set HC-12 to normal mode
   delay(20);  // Wait for the HC-12 to enter normal mode
-  while (DeviceWakeUp == 0){
-    //Send command to the nearby device for 2 seconds
-    Serial.println("Sending command over radio...");
-    for (i=1;i<2000;i++){
-      HC12.write(WebCmd);
-      delay(1);
-    }
-    //Listen from the nearby device for 2 seconds
-    Serial.println("Listening for response over radio...");
-    for (j=1;j<2000;j++){
-      x = HC12.read();
-      delay(1);
-    }
-    if(x==1){
-      DeviceWakeUp = 1;
-    }
+  for (j=1;j<10000;j++){
+    x = HC12.read();
+    delay(1);
   }
+  if(x==1){
+    WebCmd = 1;
+  }
+  digitalWrite(HC_SET,LOW); //Set HC-12 to AT command mode
+  delay(20);  // Wait for the HC-12 to enter AT Command mode
+  HC12.print("AT+SLEEP");    // Send AT Command to HC-12
+}
+
+//Respond to say the command is received
+void Respond(){
+  int i;
+  Serial.println("Sending response...");
+  HC12.print("AT+DEFAULT");    // Send AT Command to HC-12
+  digitalWrite(HC_SET,HIGH); //Set HC-12 to normal mode
+  delay(20);  // Wait for the HC-12 to enter normal mode
+  for (i=1;i<10000;i++){
+    HC12.write(WebCmd);
+    delay(1);
+  }
+  Serial.println("Response sent.");
   digitalWrite(HC_SET,LOW); //Set HC-12 to AT command mode
   delay(20);  // Wait for the HC-12 to enter AT Command mode
   HC12.print("AT+SLEEP");    // Send AT Command to HC-12
